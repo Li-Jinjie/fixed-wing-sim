b04a7a3f715d5f5044b1e3c63985f50f03f45184
Li-Jinjie <45286479+Li-Jinjie@users.noreply.github.com>
2021-01-01 15:25:47 +0800

    add tools.py

--- Kinematics_and_Dynamics/mavsim_chap3.py
+++ Kinematics_and_Dynamics/mavsim_chap3.py
@@ -6,27 +6,28 @@ mavsimPy
         1/14/2019 - RWB
 """
 import sys
+
 sys.path.append('..')
 import numpy as np
 import parameters.simulation_parameters as SIM
 
-from chap2.mav_viewer import mavViewer
-from chap3.data_viewer import dataViewer
-from chap3.mav_dynamics import mavDynamics
-
+from Coordinate_Frames.spacecraft_viewer import spacecraft_viewer
+from Kinematics_and_Dynamics.data_viewer import data_viewer
+from Kinematics_and_Dynamics.mav_dynamics import mav_dynamics
 
 # initialize the visualization
 VIDEO = False  # True==write video, False==don't write video
-mav_view = mavViewer()  # initialize the mav viewer
-data_view = dataViewer()  # initialize view of data plots
+mav_view = spacecraft_viewer()  # initialize the mav viewer
+data_view = data_viewer()  # initialize view of data plots
 if VIDEO == True:
-    from chap2.video_writer import videoWriter
-    video = videoWriter(video_name="chap3_video.avi",
+    from Coordinate_Frames.video_writer import video_writer
+
+    video = video_writer(video_name="chap3_video.avi",
                          bounding_box=(0, 0, 1000, 1000),
                          output_rate=SIM.ts_video)
 
 # initialize elements of the architecture
-mav = mavDynamics(SIM.ts_simulation)
+mav = mav_dynamics(SIM.ts_simulation)
 
 # initialize the simulation time
 sim_time = SIM.start_time
@@ -34,31 +35,27 @@ sim_time = SIM.start_time
 # main simulation loop
 print("Press Command-Q to exit...")
 while sim_time < SIM.end_time:
-    #-------vary forces and moments to check dynamics-------------
+    # -------vary forces and moments to check dynamics-------------
     fx = 10
-    fy = 0 # 10
-    fz = 0 # 10
-    Mx = 0 # 0.1
-    My = 0 # 0.1
-    Mz = 0 # 0.1
+    fy = 0  # 10
+    fz = 0  # 10
+    Mx = 0  # 0.1
+    My = 0  # 0.1
+    Mz = 0  # 0.1
     forces_moments = np.array([[fx, fy, fz, Mx, My, Mz]]).T
 
-    #-------physical system-------------
+    # -------physical system-------------
     mav.update(forces_moments)  # propagate the MAV dynamics
 
-    #-------update viewer-------------
+    # -------update viewer-------------
     mav_view.update(mav.true_state)  # plot body of MAV
-    data_view.update(mav.true_state, # true states
-                     mav.true_state, # estimated states
-                     mav.true_state, # commanded states
+    data_view.update(mav.true_state,  # true states
+                     mav.true_state,  # estimated states
+                     mav.true_state,  # commanded states
                      SIM.ts_simulation)
     if VIDEO == True: video.update(sim_time)
 
-    #-------increment time-------------
+    # -------increment time-------------
     sim_time += SIM.ts_simulation
 
 if VIDEO == True: video.close()
-
-
-
-
---
+++ tools/tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+@Author : Li-Jinjie
+@Date    : 2020/12/30
+'''
+import numpy as np
+
+
+def Quaternion2Euler():
+    # phi, theta, psi = Quaternion2Euler(self._state[6:10])
+    pass
