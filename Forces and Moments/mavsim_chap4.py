95b6e549697c21906d98735e2d89b3886d27dc3e
Li-Jinjie <lijinjie362@outlook.com>
2021-01-12 15:50:37 +0800

    add wind_flag to determine to start the wind simulation or not.

--- Forces and Moments/mavsim_chap4.py
+++ Forces and Moments/mavsim_chap4.py
@@ -6,19 +6,21 @@ mavsimPy
         1/17/2019 - RWB
 """
 import sys
+
 sys.path.append('..')
 import numpy as np
 import parameters.simulation_parameters as SIM
 
-from chap2.mav_viewer import mav_viewer
-from chap2.video_writer import video_writer
-from chap3.data_viewer import data_viewer
-from chap4.mav_dynamics import mav_dynamics
-from chap4.wind_simulation import wind_simulation
+from Coordinate_Frames.spacecraft_viewer import spacecraft_viewer
+from Coordinate_Frames.video_writer import video_writer
+from Kinematics_and_Dynamics.data_viewer import data_viewer
+from .mav_dynamics import mav_dynamics
+from .wind_simulation import wind_simulation
 
 # initialize the visualization
 VIDEO = False  # True==write video, False==don't write video
-mav_view = mav_viewer()  # initialize the mav viewer
+wind_flag = False  # True==wind, False==no wind
+mav_view = spacecraft_viewer()  # initialize the mav viewer
 data_view = data_viewer()  # initialize view of data plots
 if VIDEO == True:
     video = video_writer(video_name="chap4_video.avi",
@@ -35,32 +37,31 @@ sim_time = SIM.start_time
 # main simulation loop
 print("Press Command-Q to exit...")
 while sim_time < SIM.end_time:
-    #-------set control surfaces-------------
+    # -------set control surfaces-------------
     delta_e = -0.2
     delta_t = 0.5
     delta_a = 0.0
     delta_r = 0.005
     delta = np.array([[delta_e, delta_t, delta_a, delta_r]]).T  # transpose to make it a column vector
 
-    #-------physical system-------------
-    current_wind = wind.update()  # get the new wind vector
+    # -------physical system-------------
+    if wind_flag is True:
+        current_wind = wind.update()  # get the new wind vector
+    else:
+        current_wind = np.zeros((6, 1))
     mav.update_state(delta, current_wind)  # propagate the MAV dynamics
 
-    #-------update viewer-------------
+    # -------update viewer-------------
     mav_view.update(mav.msg_true_state)  # plot body of MAV
-    data_view.update(mav.msg_true_state, # true states
-                     mav.msg_true_state, # estimated states
-                     mav.msg_true_state, # commanded states
+    data_view.update(mav.msg_true_state,  # true states
+                     mav.msg_true_state,  # estimated states
+                     mav.msg_true_state,  # commanded states
                      SIM.ts_simulation)
     if VIDEO == True:
         video.update(sim_time)
 
-    #-------increment time-------------
+    # -------increment time-------------
     sim_time += SIM.ts_simulation
 
 if VIDEO == True:
     video.close()
-
-
-
-
