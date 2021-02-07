a99a176d0de33b457a2c7e302ee9602d7d7d961d
Li-Jinjie <lijinjie362@outlook.com>
2021-02-07 10:17:42 +0800

    finish autopilot.py

--- Autopilot/autopilot.py
+++ Autopilot/autopilot.py
@@ -6,6 +6,7 @@ autopilot block for mavsim_python
 """
 import sys
 import numpy as np
+
 sys.path.append('..')
 import parameters.control_parameters as AP
 from tools.transfer_function import TransferFunction
@@ -18,53 +19,54 @@ class AutoPilot:
     def __init__(self, ts_control):
         # instantiate lateral controllers
         self.roll_from_aileron = PdControlWithRate(
-                        kp=AP.roll_kp,
-                        kd=AP.roll_kd,
-                        limit=np.radians(45))
+            kp=AP.roll_kp,
+            kd=AP.roll_kd,
+            limit=np.radians(45))
         self.course_from_roll = PiControl(
-                        kp=AP.course_kp,
-                        ki=AP.course_ki,
-                        Ts=ts_control,
-                        limit=np.radians(30))
+            kp=AP.course_kp,
+            ki=AP.course_ki,
+            Ts=ts_control,
+            limit=np.radians(30))
         self.sideslip_from_rudder = PiControl(
-                        kp=AP.sideslip_kp,
-                        ki=AP.sideslip_ki,
-                        Ts=ts_control,
-                        limit=np.radians(45))
+            kp=AP.sideslip_kp,
+            ki=AP.sideslip_ki,
+            Ts=ts_control,
+            limit=np.radians(45))
         self.yaw_damper = TransferFunction(
-                        num=np.array([[AP.yaw_damper_kp, 0]]),
-                        den=np.array([[1, 1/AP.yaw_damper_tau_r]]),
-                        Ts=ts_control)
+            num=np.array([[AP.yaw_damper_kp, 0]]),
+            den=np.array([[1, 1 / AP.yaw_damper_tau_r]]),
+            Ts=ts_control)
 
         # instantiate lateral controllers
         self.pitch_from_elevator = PdControlWithRate(
-                        kp=AP.pitch_kp,
-                        kd=AP.pitch_kd,
-                        limit=np.radians(45))
+            kp=AP.pitch_kp,
+            kd=AP.pitch_kd,
+            limit=np.radians(45))
         self.altitude_from_pitch = PiControl(
-                        kp=AP.altitude_kp,
-                        ki=AP.altitude_ki,
-                        Ts=ts_control,
-                        limit=np.radians(30))
+            kp=AP.altitude_kp,
+            ki=AP.altitude_ki,
+            Ts=ts_control,
+            limit=np.radians(30))
         self.airspeed_from_throttle = PiControl(
-                        kp=AP.airspeed_throttle_kp,
-                        ki=AP.airspeed_throttle_ki,
-                        Ts=ts_control,
-                        limit=1.0)
+            kp=AP.airspeed_throttle_kp,
+            ki=AP.airspeed_throttle_ki,
+            Ts=ts_control,
+            limit=1.0)
         self.commanded_state = MsgState()
 
     def update(self, cmd, state):
+        # state is MsgState(), store a lot of information
 
         # lateral autopilot
-        phi_c =
-        delta_a =
-        delta_r =
+        phi_c = self.course_from_roll.update(cmd.course_command, state.chi)
+        delta_a = self.roll_from_aileron.update(phi_c, state.phi, state.p)  # phi_dot = p
+        delta_r = self.sideslip_from_rudder.update(0, state.beta)
 
         # longitudinal autopilot
-        h_c =
-        theta_c =
-        delta_e =
-        delta_t =
+        h_c = cmd.altitude_command
+        theta_c = self.altitude_from_pitch.update(h_c, state.h)
+        delta_e = self.pitch_from_elevator.update(theta_c, state.theta, state.q)  # theta_dot is approximately ~ q
+        delta_t = self.airspeed_from_throttle.update(cmd.airspeed_command, state.Va)  # delta_t_* is not known
 
         # construct output and commanded states
         delta = np.array([[delta_e], [delta_a], [delta_r], [delta_t]])
